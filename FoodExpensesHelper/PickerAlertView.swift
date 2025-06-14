78ce577b98b786dff6e1fa29891872b57a51216b
Aoi-Kobayashi <oosabaaoi@gmail.com>
2025-06-15 01:37:34 +0900

    残高で自動モード切替

---
+++ FoodExpensesHelper/PickerAlertView.swift
@@ -0,0 +1,18 @@
+//
+//  PickerAlertView.swift
+//  FoodExpensesHelper
+//
+//  Created by AoiKobayashi on 2025/06/08.
+//
+
+import SwiftUI
+
+struct PickerAlertView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PickerAlertView()
+}
